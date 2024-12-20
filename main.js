154115bf0ed3164ac2a555d151d4a7410b6c2bd7
Leon <tahjib.leon399@gmail.com>
2024-12-21 00:35:16 +0600

    nested function scope

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-const x = 10;
+// nested function scope
+let a = 10;
+function outer() {
+    let b = 20;
+    function inner() {
+        let c = 30;
+        console.log(a); // 10
+        console.log(b); // 20
+        console.log(c); // 30
+    }
+    inner();
+}
 
-console.log(x);
+outer();
