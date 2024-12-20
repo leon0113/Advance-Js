730b9cf02c02991804f6ed5b2d80ce78a7ac0a7c
Leon <tahjib.leon399@gmail.com>
2024-12-21 01:00:30 +0600

    curring

---
+++ closure.js
@@ -0,0 +1,16 @@
+function outer() {
+    let counter = 0;
+    function inner() {
+        counter++;
+        console.log(counter);
+    }
+
+    return inner;
+}
+
+const fn = outer();
+
+fn();
+fn();
+
+// outer();
